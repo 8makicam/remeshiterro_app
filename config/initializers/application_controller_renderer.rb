5d5e21024f379614ac7d06346a2302cef014fd69
Yuta Yamaki <8makicam@gmail.com>
2021-02-07 13:02:22 +0000

    chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2f1f9c10883c1a03d963913c79f8f58ca2afb6fe4b24ef5c6a070afa5d6bb34a1abbee8826d3e9ad9798ea80c72100a8f47dcae3ca91e1cc33379766ac671ab2'
+
